f8967c92c056621f7ae1822bf373ca71556db9b2
Devon Jones <devon.jones@gmail.com>
2020-05-07 14:05:57 -0600

    some minor updates

--- bin/run_creatures.sh
+++ bin/run_creatures.sh
@@ -2,5 +2,8 @@
 set -e
 
 source dir.conf
-ls $WEB_DIR/Monsters/Monsters.aspx.ID_* | xargs ./creature_parse -o $DATA_DIR
-#./creature_parse -o $DATA_DIR $WEB_DIR/Monsters/Monsters.aspx.ID_5
+#./creature_parse -ds $WEB_DIR/Monsters/Monsters.aspx.ID_333
+#./creature_parse -d $WEB_DIR/Monsters/Monsters.aspx.ID_*
+./creature_parse -o $DATA_DIR $WEB_DIR/Monsters/Monsters.aspx.ID_*
+
+#169
