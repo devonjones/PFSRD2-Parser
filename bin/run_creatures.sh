9111d3778a865ec174369db4c48b0f62c46be612
Devon Jones <devon.jones@gmail.com>
2020-12-14 14:28:54 -0700

    switched errors to a log

--- bin/run_creatures.sh
+++ bin/run_creatures.sh
@@ -6,21 +6,20 @@ source dir.conf
 #./creature_parse -d $WEB_DIR/Monsters/Monsters.aspx.ID_*
 #./creature_parse -o $DATA_DIR $WEB_DIR/Monsters/Monsters.aspx.ID_*
 
-rm errors.creatures.new
-touch errors.creatures.new
+rm errors.creatures.log
 
 if test -f "errors.creatures"; then
 	cat errors.creatures | while read i
 	do
 		if ! ./creature_parse -o $DATA_DIR $i ; then
-			echo $i >> errors.creatures.new
+			echo $i >> errors.creatures.log
 		fi
 	done
 else
 	for i in `ls $WEB_DIR/Monsters/Monsters.aspx.ID_* | msort -j -q -l -n 1 -c hybrid`
 	do
 		if ! ./creature_parse -o $DATA_DIR $i ; then
-			echo $i >> errors.creatures.new
+			echo $i >> errors.creatures.log
 		fi
 	done
 fi
--- bin/run_npcs.sh
+++ bin/run_npcs.sh
@@ -6,21 +6,20 @@ source dir.conf
 #./npc_parse -d $WEB_DIR/NPCs/NPCs.aspx.ID_*
 #./npc_parse -o $DATA_DIR $WEB_DIR/NPCs/NPCs.aspx.ID_*
 
-rm errors.npc.new
-touch errors.npc.new
+rm errors.npc.log
 
 if test -f "errors.npc"; then
 	cat errors.npc | while read i
 	do
 		if ! ./npc_parse -o $DATA_DIR $i ; then
-			echo $i >> errors.npc.new
+			echo $i >> errors.npc.log
 		fi
 	done
 else
 	for i in `ls $WEB_DIR/NPCs/NPCs.aspx.ID_* | msort -j -q -l -n 1 -c hybrid`
 	do
 		if ! ./npc_parse -o $DATA_DIR $i ; then
-			echo $i >> errors.npc.new
+			echo $i >> errors.npc.log
 		fi
 	done
 fi
